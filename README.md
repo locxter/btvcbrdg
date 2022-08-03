c308598ebaffd4055cba69f82fb48d67632fddd1
locxter <54595101+locxter@users.noreply.github.com>
2022-08-03 11:39:19 +0200

    Uploaded already existing files.

---
+++ README.md
@@ -0,0 +1,23 @@
+# btvcbrdg
+
+## Overview
+
+![Overview image](overview.png)
+
+This is an ESP8266 bridge for communicating to Neato Botvac robot vacuums over the network.
+
+## Dependencies
+
+I generally try to minimize dependencies, but I'm a one man crew and can therefore only support Debian as I'm running it myself. Anyway, you need to have the following packages installed for everything to work properly:
+
+- Arduino IDE as a way to compile the Arduino code. Install it with `sudo apt install arduino`.
+
+## How to build it
+
+![Wiring diagram](wiring.png)
+
+After flashing the firmware to the ESP and opening your vacumm robot, find the three golden soldering pads in the top left of the mainboard and solder wires between them and the ESP as shown in the wiring diagram above. That is it!
+
+## How to use it
+
+After turning on your Botvac and giving the ESP roughly half a minute to boot up, you can send `POST` requests with basic authentication and the `command` form field to talk to your vacuum robot. Alternatively you can also simply open it's web interface and send commands through it.
---
+++ btvcbrdg.ino
@@ -0,0 +1,64 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+// WiFi, user authentication and serial related constants
+const String WIFI_SSID = "";
+const String WIFI_PASSWORD = "";
+const String HOSTNAME = "btvcbrdg";
+const char* USERNAME = "btvcbrdg";
+const char* PASSWORD = "btvcbrdg";
+const int BAUD_RATE = 115200;
+
+// Server object
+ESP8266WebServer server(80);
+
+// Setup function
+void setup(void) {
+    // Initialize serial and WiFi connection
+    Serial.begin(BAUD_RATE);
+    Serial.setTimeout(10000);
+    Serial.readString();
+    Serial.setTimeout(1000);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+    // Initialize mDNS
+    MDNS.begin(HOSTNAME);
+    // Initialize server
+    server.on("/", HTTP_GET, [&]() {
+        if (!server.authenticate(USERNAME, PASSWORD)) {
+            server.requestAuthentication();
+        } else {
+            String content = "<!DOCTYPE html><html lang=\"en\"><head><title>btvcbrdg</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta name=\"author\" content=\"locxter\"><meta name=\"description\" content=\"This is an ESP8266 bridge for communicating to Neato Botvac robot vacuums over the network.\"><link rel=\" stylesheet\" href=\"https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css\"></head><body><header><h1>btvcbrdg</h1></header><main><form method=\"post\"><label for=\"command\">Command:</label><input id=\"command\" type=\"text\" name=\"command\"><input type=\"submit\" value=\"Send\"></form></main><footer><p>2022 locxter</p></footer></body></html>";
+            server.send(200, "text/html", content);
+        }
+    });
+    server.on("/", HTTP_POST, [&]() {
+        if (!server.authenticate(USERNAME, PASSWORD)) {
+            server.requestAuthentication();
+        } else {
+            String content = "";
+            if (server.hasArg("command")) {
+                Serial.print(server.arg("command"));
+                Serial.print('\n');
+                Serial.readStringUntil('\n');
+                content = Serial.readStringUntil('\x1a');
+            }
+            server.send(200, "text/plain", content);
+        }
+    });
+    server.onNotFound([&]() {
+        server.send(404, "text/plain", "Resource not found");
+    });
+    server.begin();
+}
+
+// Main function
+void loop(void) {
+    server.handleClient();
+    MDNS.update();
+}
